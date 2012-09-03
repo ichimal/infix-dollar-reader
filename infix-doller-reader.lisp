00003c582578c08ed613225b24fa8d11fdebd664
Shingo SUZUKI <r2.ichimal@gmail.com>
2012-09-04 03:13:11 +0900

    Add enabler and disabler
    CAUTION: enabler and disabler could modify global readtable directly.

--- infix-doller-reader.lisp
+++ infix-doller-reader.lisp
@@ -3,11 +3,15 @@
 
 (defpackage :infix-doller-reader
   (:nicknames :idoller)
-  (:shadow #:infix-doller-reader)
-  (:use :cl) )
+  (:shadow #:infix-doller-reader #:*original-readtable*)
+  (:use :cl)
+  (:export #:use-infix-doller #:unuse-infix-doller) )
 
 (eval-when (:load-toplevel :compile-toplevel :execute)
   (in-package :infix-doller-reader) )
+
+(defvar *original-readtable* nil
+  "Saving original readtable" )
 
 (defun infix-doller-reader (stream char)
   (declare (ignore char))
@@ -16,5 +20,24 @@
     (unread-char #\) stream)
     entity ))
 
-(set-macro-character #\$ #'infix-doller-reader)
+(defun use-infix-doller (&optional print-note)
+  (declare (ignorable print-note))
+  "Enable infix $ operator.
+CAUTION: enable-infix-doller could modify *readtable* globally."
+  (push *readtable* *original-readtable*)
+  (setf *readtable* (copy-readtable) )
+  (set-macro-character #\$ #'infix-doller-reader)
+  (when print-note
+    (format t "~&NOTE: set #\\$ as macro character globally.~%") ))
+
+(defun unuse-infix-doller (&optional print-note)
+  "Restore whole *readtable*.
+CAUTION: unuse-infix-doller could rollback all of the changes on *readtable*
+after invoking enable-infix-doller."
+  (unless (readtablep (car *original-readtable*))
+    (error "No readtable modified") )
+  (setf *readtable* (pop *original-readtable*))
+  (when print-note
+    (format t "~&NOTE: restored *readtable* globally.~%") ))
+
 
